43ccc09ceda71c19638e1cdd17adbdc92d7fa4c7
Ivan Naskov <ivannaskov@Ivans-MacBook-Air.local>
2024-10-08 18:19:35 +0200

    refactor: check if input is empty before creating a task and add a new function for appending tasks

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
-const toggleInputButtons = document.querySelectorAll('.toggle-input-btn');
-
 const fetchTasks = () => {
-    console.log('asfas')
     const sections = ["todo", "inprogress", "done"];
+
     sections.forEach(section => {
         const tasks = JSON.parse(localStorage.getItem(section)) || [];
         const tasksList = document.getElementById(section + "-list");
-
+        
         tasks.forEach(task => {
-            const listItem = document.createElement("li");
-            listItem.textContent = task;
-            tasksList.append(listItem);
+            appendTask(tasksList, task);
         });
     });
 };
 
+const toggleInputButtons = document.querySelectorAll('.toggle-input-btn');
+
 toggleInputButtons.forEach(button => {
     button.addEventListener('click', () => {
         button.style.display = "none";
+
         const sectionId = button.id.split("-")[0];
         const inputSection = document.getElementById(sectionId + "-input-section");
 
         inputSection.style.display = "flex";
         const closeBtn = document.getElementById(sectionId + "-close-btn");
 
-        closeBtn.addEventListener('click', () => {
-            closeInputSection(button, inputSection);
-        });
+        closeBtn.addEventListener('click', () => closeInputSection(button, inputSection));
         
         addCardBtn = inputSection.querySelector('button');
         addCardBtn.addEventListener('click', () => {
             saveTask(sectionId, inputSection.children[0].value);
-            inputSection.children[0].value = "";
             closeInputSection(button, inputSection);
         });
     });
 });
 
-const closeInputSection = (button, inputSection) => {
-    button.style.display = "block";
-    inputSection.style.display = "none";
+const appendTask = (tasksList, task) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = task;
+    tasksList.append(listItem);
 };
 
 const saveTask = (sectionId, task) => {
-    const tasks = JSON.parse(localStorage.getItem(sectionId)) || [];
-    tasks.push(task);
-    localStorage.setItem(sectionId, JSON.stringify(tasks));
+    if (task.trim()) {
+        const tasksList = document.getElementById(sectionId + "-list");
+        const tasks = JSON.parse(localStorage.getItem(sectionId)) || [];
+        tasks.push(task);
+        localStorage.setItem(sectionId, JSON.stringify(tasks));
+        appendTask(tasksList, task);
+    }
+};
+
+const closeInputSection = (button, inputSection) => {
+    inputSection.children[0].value = "";
+    button.style.display = "block";
+    inputSection.style.display = "none";
 };
 
 document.addEventListener('DOMContentLoaded', fetchTasks);
