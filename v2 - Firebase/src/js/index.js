c68ccd1b148e97c22a2350b5d9021bfc78850835
Ivan Naskov <ivannaskov15@gmail.com>
2024-10-12 19:04:54 +0200

    fix: fix redirecting error by checking pathname

--- v2 - Firebase/src/js/index.js
+++ v2 - Firebase/src/js/index.js
@@ -5,7 +5,9 @@ import { auth } from "./firebase";
 document.addEventListener("DOMContentLoaded", () => {
   auth.onAuthStateChanged((user) => {
     if (!user) {
-      window.location.href = "login.html";
+      if (window.location.pathname === "/index.html") {
+        window.location.href = "login.html";
+      }
     } else {
       fetchTasks();
     }
