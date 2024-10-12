0ffab62116e5a4351a1216758218a0b73f0350f3
Ivan Naskov <ivannaskov15@gmail.com>
2024-10-12 16:34:17 +0200

    feat: redirect user to login page if not logged in

--- v2 - Firebase/src/index.js
+++ v2 - Firebase/src/index.js
@@ -1,4 +1,13 @@
 import { fetchTasks } from "./tasks";
-import './styles.css';
+import "./styles.css";
+import { auth } from "./firebase";
 
-document.addEventListener("DOMContentLoaded", fetchTasks);
+document.addEventListener("DOMContentLoaded", () => {
+  auth.onAuthStateChanged((user) => {
+    if (!user) {
+      window.location.href = "login.html";
+    } else {
+      fetchTasks();
+    }
+  });
+});
