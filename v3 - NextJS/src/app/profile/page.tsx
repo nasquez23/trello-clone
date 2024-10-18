17ecd3b5b94cfef1b270a2713c5d2feb0d771461
Ivan Naskov <ivannaskov15@gmail.com>
2024-10-18 12:35:09 +0200

    feat: add back to home button on profile page

--- v3 - NextJS/src/app/profile/page.tsx
+++ v3 - NextJS/src/app/profile/page.tsx
@@ -1,10 +1,29 @@
 import Header from "@/components/Header";
 import ProfileCard from "@/components/ProfileCard";
+import { ArrowBack } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import Link from "next/link";
 
 const ProfilePage = () => {
   return (
     <>
       <Header />
+      <Link href="/">
+        <Button
+          sx={{
+            textTransform: "none",
+            color: "white",
+            fontSize: "1.1rem",
+            marginLeft: "1.5rem",
+            "&:hover": {
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+            },
+          }}
+          startIcon={<ArrowBack sx={{ fontSize: "1.1rem" }} />}
+        >
+          Back To Home
+        </Button>
+      </Link>
       <ProfileCard />
     </>
   );
